2fbe7de4d4ab57bfa8ba1906c5ef32f4b2d73ba3
Batuhan YILMAZ <100535577+batuhanylmz1421@users.noreply.github.com>
2022-03-28 19:29:30 +0300

    week 4

--- Patika.Dev-Clone/src/com/patikadev/Helper/Config.java
+++
@@ -1,9 +0,0 @@
-package com.patikadev.Helper;
-
-public class Config {
-    public static final String PROJECT_TITLE="Patika.dev";
-    public static final String DB_URL="jdbc:mysql://localhost/patika";
-    public static final String DB_USERNAME = "root";
-    public static final String DB_PASSWORD = "";
-}
-
